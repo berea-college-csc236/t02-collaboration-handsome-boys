06d21c4c9903533257d33ca2d20478ebd0e578a1
Sugarragchaa Tumur-Ochir <tumurochirs@berea.edu>
2024-08-26 10:46:10 -0400

    mine

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
     See https://en.wikipedia.org/wiki/Rubber_duck_debugging 
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
+    Edits made by:
+    TODO: <Sugarragchaa1> -- to be added by student 1
+    TODO: <Add name of student 2 Here> -- to be added by student 2
+    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+ */
+#include <iostream>
+using namespace std;
+
 int main() {
 
     string animal = 0;
